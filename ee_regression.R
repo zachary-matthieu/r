f3dcbc0911a1e4c52a8df9c037725bc8cc829c30
Zachary Young <129686466+zachary-matthieu@users.noreply.github.com>
2023-06-13 13:48:17 -0400

    Add files via upload

---
+++ ee_regression.R
@@ -0,0 +1,11 @@
+install.packages("tidyverse")
+
+library(tidyverse)
+library(ggplot2)
+
+data(iris)
+
+virginica <- iris[101:150,]
+
+ggplot(data = virginica, aes(virginica$Sepl.Length, virginica$Sepal.Width)) + geom_point()
+       
