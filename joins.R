a43337f866d2880f68126f53fb030823c1812f9d
Zachary Young <129686466+zachary-matthieu@users.noreply.github.com>
2023-07-07 10:35:58 -0400

    Add files via upload

---
+++ joins.R
@@ -0,0 +1,39 @@
+#### Data CLeanining ----
+
+library(gapminder)
+library(tidyverse)
+
+data(gapminder)
+
+df <- gapminder
+
+df %>%
+  arrange(desc(gdpPercap)) %>%
+  head(n=15)
+
+outliers <- df %>%
+  filter(country == "Hong Kong, China")
+
+df_clean <- df %>%
+  filter(country != "Hong Kong, China")
+
+str(df_clean)
+
+df_clean$country <- as.character(df_clean$country)
+df_clean$continent <- as.character(df_clean$country)
+
+str(df_clean)
+
+summary(df_clean)
+
+#### Joins ----
+
+country <- gapminder %>%
+  select(country, year, lifeExp, pop, gdpPercap)
+
+continent <- gapminder %>%
+  select(continent, year)
+
+df_j1 <- left_join(country, continent, by = 'year')
+
+df_j2 <- inner_join(country, continent, by = 'year')
